8788172908567f407e369545c68b9797e3db9f13
adictonator <adityabhaskarsharma@gmail.com>
2023-04-28 23:58:06 +0530

    added payment related logic WIP

--- src/components/PaymentMethods.jsx
+++ src/components/PaymentMethods.jsx
@@ -1,17 +1,37 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import axios from '@/lib/axios'
 
-const savedMethods = [{ name: 'Personal Card' }, { name: 'Business Card Visa' }]
+export default ({ setShowCardForm }) => {
+	const [methods, setMethods] = useState([])
+	const [selected, setSelected] = useState()
 
-export default () => {
-	const [selected, setSelected] = useState(savedMethods[0])
+	useEffect(() => {
+		// get all the methods here.
+		//axios.get('/payment/gateways').then(({ data }) => {
+		//	setMethods(data)
+		//	setSelected(data[0].cardHolder)
+		//})
+	}, [])
+
+	const handleMethodChange = selected => {
+		setSelected(selected)
+
+		if ('new' === selected) {
+			setShowCardForm(true)
+		} else {
+			setShowCardForm(false)
+		}
+	}
 
 	return (
-		<Listbox value={selected} onChange={setSelected}>
+		<Listbox value={selected ?? null} onChange={handleMethodChange}>
 			<div className="relative mt-1">
 				<Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-					<span className="block truncate">{selected.name}</span>
+					<span className="block truncate">
+						{selected ?? 'No method found.'}
+					</span>
 					<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
 						<ChevronUpDownIcon
 							className="h-5 w-5 text-gray-400"
@@ -25,7 +45,7 @@ export default () => {
 					leaveFrom="opacity-100"
 					leaveTo="opacity-0">
 					<Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-						{savedMethods.map((person, personIdx) => (
+						{methods.map((method, personIdx) => (
 							<Listbox.Option
 								key={personIdx}
 								className={({ active }) =>
@@ -35,7 +55,7 @@ export default () => {
 											: 'text-gray-900'
 									}`
 								}
-								value={person}>
+								value={method.cardHolder}>
 								{({ selected }) => (
 									<>
 										<span
@@ -44,7 +64,7 @@ export default () => {
 													? 'font-medium'
 													: 'font-normal'
 											}`}>
-											{person.name}
+											{method.cardHolder}
 										</span>
 										{selected ? (
 											<span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
