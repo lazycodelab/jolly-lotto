715fe42da206f098a0d8355c2b705a649d375a06
adictonator <adityabhaskarsharma@gmail.com>
2023-12-12 16:35:28 +0530

    updated cutoff timer logic

--- components/play-button.jsx
+++ components/play-button.jsx
@@ -1,10 +1,17 @@
 import cx from 'classnames'
 
-export default ({ mobile = false }) => {
+export default ({ mobile = false, disabled = false }) => {
+	//const [disabled ]
 	return (
 		<button
+			disabled={disabled}
 			className={cx(
-				'rounded-lg bg-gradient-to-r from-orange-400 to-orange-500 text-white shadow-md shadow-orange-700 hover:from-orange-500 hover:to-orange-400',
+				'rounded-lg text-white shadow-md',
+				{
+					'bg-gray-300 cursor-not-allowed': disabled,
+					'bg-gradient-to-r from-orange-400 to-orange-500 shadow-orange-700 hover:from-orange-500 hover:to-orange-400':
+						!disabled,
+				},
 				{
 					'px-2 py-3 text-sm': mobile === true,
 					'px-7 py-1 text-sm': mobile === false,
